68b206ab15804e57c4166510223c7bea249fb664
RenanFernandoRuiz <renanfernandor908@gmail.com>
2024-10-24 11:05:50 -0300

    .

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import"./Header.css"
+
+const Header = () => {
+  return (
+    <nav className="navbar navbar-expand-lg"> 
+     <div className="master container-fluid p-0">
+      <h2>Contato</h2>
+      
+     </div>
+  
+    </nav>
+  )
+}
+
+export default Header
